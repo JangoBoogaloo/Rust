77da1328ace677becbc73df0f027bb204a7213fc
Xiang Guo <xiangguoxg@gmail.com>
2022-09-24 18:05:07 -0400

    slice

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
-    let mut s = String::from("hello");
+    let s = String::from("hello world");
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{} and {}", r1, r2);
-    // variables r1 and r2 will not be used after this point
+    let word = first_word(&s);
 
-    let r3 = &mut s; // no problem
-    println!("{}", r3);
+    //s.clear(); // error!
+
+    println!("the first word is: {}", word);
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
