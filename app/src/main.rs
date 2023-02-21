4edace1522d97a55f67d11bc896a4fe551acb55a
Xiang Guo <xiangguoxg@gmail.com>
2023-02-21 16:17:31 -0500

    slice

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello World!");
+    let s = String::from("hello world A B C");
+    let word = first_word(&s[0..10]);
+    println!("{word}");
+    let word = first_word(&s[..]);
+    println!("{word}");
+    let word = first_word(&s);
+    println!("{word}");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
 }
