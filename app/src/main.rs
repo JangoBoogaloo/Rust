1890ecf96ac9b80baa2042e81b9e08addfca3e51
Xiang Guo <xiangguoxg@gmail.com>
2023-02-21 15:02:38 -0500

    borrow

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello World!");
+    let s = String::from("hello");
+    borrow(&s);
+    borrowAndCHange(&s);
 }
+
+fn borrow(some_string: &String) {
+    println!("{some_string}");
+}
+
+fn borrowAndCHange(some_string: &String) {
+    some_string.push_str(", world");
+}
+
