d80eb402aa746f9a9ac12b284e23d71ad5ea9c33
Xiang Guo <xiangguoxg@gmail.com>
2023-02-22 15:43:16 -0500

    Update main.rs

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,8 +1,9 @@
 fn main() {
     let some_number: Option<i32> = Some(5);
     let some_char = Some('e');
+    println!("{}", some_number.unwrap());
 
     let absent_number: Option<i32> = None;
-    let efg = absent_number + some_number;
+    println!("{}", absent_number.unwrap());
     println!("Hello World");
 }
