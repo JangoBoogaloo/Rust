df6010984bda259a2fa1c3c9361a8380dc186410
Xiang Guo <xiangguoxg@gmail.com>
2023-02-22 13:32:30 -0500

    struct example and debug printing

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,30 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    println!("Hello World!");
+    let mut rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    update(&mut rect1, 40, 20);
+    println!("{:#?}",rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    //debug print
+    dbg!(&rect1);
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn update(rectangle: &mut Rectangle, width: u32, height: u32) {
+    rectangle.width = width;
+    rectangle.height = height;
 }
