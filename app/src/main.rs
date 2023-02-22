fd3a6acb7ff10009e484142b2b72316c5e22c058
Xiang Guo <xiangguoxg@gmail.com>
2023-02-22 15:37:19 -0500

    option enum

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello World!");
+    let some_number: Option<i32> = Some(5);
+    let some_char = Some('e');
+
+    let absent_number: Option<i32> = None;
+    let efg = absent_number + some_number;
+    println!("Hello World");
 }
