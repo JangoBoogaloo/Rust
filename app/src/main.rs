ebf8f610b63aae06d49862ad98643bb3afca4d67
Xiang Guo <xiangguoxg@gmail.com>
2023-02-22 14:23:11 -0500

    method

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,49 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+
+    //associate functions
+    fn area(&self) -> u32 {
+        return self.width*self.height;
+    }
+
+    //associate functions: like a constructor
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+impl Rectangle {
+
+    //associate functions
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 fn main() {
-    println!("Hello World!");
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle::square(30);
+
+    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    dbg!(&rect2);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect2.area()
+    );
 }
