8340fce4cb61f9ab3d6f28645e3517d3dd45e52a
Xiang Guo <xiangguoxg@gmail.com>
2023-02-23 13:06:27 -0500

    hashmap

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,42 @@
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello World!");
+
+    // Create
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+    println!("{:#?}", scores);
+
+    let team_name = String::from("Blue");
+    let score = scores.get("Blue").copied().unwrap_or(0);
+
+    println!("{:#?}", score);
+
+    //Arbitrary order
+    for (key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+
+
+    let mut scores = HashMap::new();
+    scores.insert(String::from("Blue"), 10);
+
+    //insert if not exist
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+
+    println!("{:?}", scores);
+
+    let text = "hello world wonderful world";
+
+    let mut map = HashMap::new();
+
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+
+    println!("{:?}", map);
 }
