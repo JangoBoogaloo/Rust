1bd922e50c0ce21e1e0d69380c024cc1945a8a31
Xiang Guo <xiangguoxg@gmail.com>
2023-02-23 10:09:18 -0500

    create and access vector

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello World!");
+    let v = vec![1, 2, 3, 4, 5];
+
+    let third: &i32 = &v[2];
+    println!("The third element is {third}");
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    //Not panic, return None
+    let does_not_exist = v.get(100);
+    //panic
+    let does_not_exist = &v[100];
+
 }
