58e679d012845add847917327c0c2802f7e085fa
Xiang Guo <xiangguoxg@gmail.com>
2023-02-23 15:17:21 -0500

    implement trait

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,49 @@
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
 fn main() {
-    println!("Hello World!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+    let news_article = NewsArticle {
+        headline: String::from("This is a headline"),
+        location: String::from("Canada"),
+        author: String::from("Author"),
+        content: String::from("Content")
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+    println!("2 new article: {}", news_article.summarize());
 }
