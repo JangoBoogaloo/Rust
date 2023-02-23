ee1e01956e0533e402590c23ec98966668a0636c
Xiang Guo <xiangguoxg@gmail.com>
2023-02-23 14:17:15 -0500

    ? operator

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,18 @@
+use std::fs::{File, remove_file};
+use std::io::{self, Read};
+
+fn read_username_from_file(file_name: &str) -> Result<String, io::Error> {
+    let mut username = String::new();
+
+    File::open(file_name)?.read_to_string(&mut username)?;
+
+    Ok(username)
+}
+
 fn main() {
-    println!("Hello World!");
+    let file_name = "Hellow.txt";
+    File::create(file_name).expect("Fail to create file");
+    let username = read_username_from_file(file_name).expect("File does not exist");
+    remove_file(file_name).expect("Fail to delete file");
+    print!("UserName: {username}");
 }
