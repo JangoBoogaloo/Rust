c2404dddb973a9b415a36bd9a22dbf131b327454
Xiang Guo <xiangguoxg@gmail.com>
2023-02-24 14:39:59 -0500

    a sample cli

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,28 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello World!");
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("[Error]: {}", err);
+        process::exit(1);
+    });
+
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args.get(1).expect("Please enter query argument").clone();
+        let filename = args.get(2).expect("Please enter file path argument").clone();
+
+        Ok(Config { query, filename })
+    }
 }
