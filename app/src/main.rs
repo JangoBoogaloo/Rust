11c4271e4a7ecfd7b5248245f41bd75db5a32638
Jango <xiangguoxg@gmail.com>
2023-05-18 09:44:45 -0400

    ugsr

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,22 +1,19 @@
-use clap::Parser;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
-
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
-}
+use clap::{arg, Command};
 
 fn main() {
-    let args = Args::parse();
-
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    let matches = Command::new("ugsr")
+        .version("0.1.0")
+        .author("Kevin K. <kbknapp@gmail.com>")
+        .about("rust version of UGS CLI")
+        .arg(arg!(--two <VALUE>).required(true))
+        .arg(arg!(--one <VALUE>).required(true))
+        .get_matches();
+    println!(
+        "two: {:?}",
+        matches.get_one::<String>("two").expect("required")
+    );
+    println!(
+        "one: {:?}",
+        matches.get_one::<String>("one").expect("required")
+    );
 }
